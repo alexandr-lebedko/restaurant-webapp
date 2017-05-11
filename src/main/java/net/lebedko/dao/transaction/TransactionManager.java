19c7c5de510f5c1f8993b066e7816d6ec5202b5b
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-05-11 11:23:23 +0200

    replaced DataAccessException on TransactionExceptiton in method declarations

--- src/main/java/net/lebedko/dao/transaction/TransactionManager.java
+++ src/main/java/net/lebedko/dao/transaction/TransactionManager.java
@@ -1,6 +1,7 @@
 package net.lebedko.dao.transaction;
 
 import net.lebedko.dao.exception.DataAccessException;
+import net.lebedko.dao.exception.TransactionException;
 
 import java.util.concurrent.Callable;
 
@@ -11,33 +12,33 @@ import java.util.concurrent.Callable;
 public interface TransactionManager {
 
 
-    void begin() throws DataAccessException;
+    void begin() throws TransactionException;
 
-    void commit() throws DataAccessException;
+    void commit() throws TransactionException;
 
-    void rollback() throws DataAccessException;
+    void rollback() throws TransactionException;
 
 
-    default void tx(Runnable runnable) throws DataAccessException {
+    default void tx(Runnable runnable) throws TransactionException{
         try {
             begin();
             runnable.run();
         } catch (Exception e) {
             rollback();
-            throw new DataAccessException(e);
+            throw new TransactionException(e);
         } finally {
             commit();
         }
     }
 
-    default <T> T tx(Callable<T> callable) throws DataAccessException {
+    default <T> T tx(Callable<T> callable) throws TransactionException{
         T result;
         try {
             begin();
             result = callable.call();
         } catch (Exception e) {
             rollback();
-            throw new DataAccessException(e);
+            throw new TransactionException(e);
         } finally {
             commit();
         }
