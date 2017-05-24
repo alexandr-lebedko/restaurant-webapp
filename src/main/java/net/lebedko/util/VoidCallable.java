ac37d48710716592170f8b9ddf88aff7ba4f8b15
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-05-24 12:22:48 +0200

    addded new class for executing void tasks

---
+++ src/main/java/net/lebedko/util/VoidCallable.java
@@ -0,0 +1,10 @@
+package net.lebedko.util;
+
+import java.util.concurrent.Callable;
+
+/**
+ * alexandr.lebedko : 11.05.2017.
+ */
+public interface  VoidCallable {
+     void call() throws Exception;
+}
