a1894dcafbc3c1aaa08f7f1a4ac47ae225c01481
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-05-24 15:26:14 +0200

    Override constructor

--- src/main/java/net/lebedko/dao/exception/DataAccessException.java
+++ src/main/java/net/lebedko/dao/exception/DataAccessException.java
@@ -24,5 +24,4 @@ public class DataAccessException extends Exception{
     public DataAccessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
-
 }
