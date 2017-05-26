2051bbfd00b199584bae07458c8f0027f5b72bb0
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-05-26 08:23:10 +0200

    added new methods to the UserDao: 'insertReqistrationKey' and 'findByRegistrationKey'

--- src/main/java/net/lebedko/dao/UserDao.java
+++ src/main/java/net/lebedko/dao/UserDao.java
@@ -4,10 +4,17 @@ import net.lebedko.dao.exception.DataAccessException;
 import net.lebedko.entity.general.EmailAddress;
 import net.lebedko.entity.user.User;
 
+import java.util.UUID;
+
 /**
  * alexandr.lebedko : 21.04.2017.
  */
 public interface UserDao extends GenericDao<User> {
 
     User findByEmail(EmailAddress email) throws DataAccessException;
+
+    void insertRegistrationKey(User user, UUID key);
+
+    User findByRegistrationKey(UUID key);
+
 }
