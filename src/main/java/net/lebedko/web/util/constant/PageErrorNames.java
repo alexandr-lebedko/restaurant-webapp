ef58d0cd7f21cae20a7e416c8651570f9e3acbd4
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-06-29 00:39:12 +0200

    added new page titles

--- src/main/java/net/lebedko/web/util/constant/PageErrorNames.java
+++ src/main/java/net/lebedko/web/util/constant/PageErrorNames.java
@@ -4,5 +4,8 @@ package net.lebedko.web.util.constant;
  * alexandr.lebedko : 15.06.2017
  */
 public class PageErrorNames {
-private PageErrorNames(){}
+    private PageErrorNames() {
+    }
+
+    public static final String USER_EXISTS = "page.error.userExists";
 }
--- src/main/java/net/lebedko/web/util/constant/Views.java
+++ src/main/java/net/lebedko/web/util/constant/Views.java
@@ -7,4 +7,6 @@ package net.lebedko.web.util.constant;
 public class Views {
     public static final String LOGIN = "login";
     public static final String REGISTRATION = "registration";
+    public static final String MAIN_GUEST = "user/main";
+    public static final String ERROR_500 = "error500";
 }
