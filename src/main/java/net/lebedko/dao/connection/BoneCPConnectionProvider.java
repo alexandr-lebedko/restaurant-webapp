8d9684a2772c532aab17106a9542b7dab3df62d0
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-06-30 14:56:23 +0200

    added logging

--- src/main/java/net/lebedko/dao/connection/BoneCPConnectionProvider.java
+++ src/main/java/net/lebedko/dao/connection/BoneCPConnectionProvider.java
@@ -2,6 +2,8 @@ package net.lebedko.dao.connection;
 
 import com.jolbox.bonecp.BoneCP;
 import com.jolbox.bonecp.BoneCPConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.util.PropertiesUtil;
 
 import java.sql.Connection;
@@ -14,6 +16,7 @@ import static net.lebedko.util.PropertyUtil.loadProperties;
  * alexandr.lebedko : 30.06.2017.
  */
 public class BoneCPConnectionProvider implements ConnectionProvider {
+    private static final Logger logger = LogManager.getLogger();
     private BoneCP cp;
 
     public BoneCPConnectionProvider() {
@@ -26,12 +29,14 @@ public class BoneCPConnectionProvider implements ConnectionProvider {
         try {
             cp = new BoneCP(config);
         } catch (SQLException e) {
-            throw new RuntimeException("Could not create connection pool");
+            logger.error("Failed to create BoneCP connection pool :", e);
+            throw new RuntimeException("Could not create connection pool", e);
         }
     }
 
     @Override
     public Connection getConnection() throws SQLException {
+        logger.debug("New connection requested");
         return cp.getConnection();
     }
 }
