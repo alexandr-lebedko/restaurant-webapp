7d74ba2d434b621892baed08ef020a8e45a2a05a
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-07-05 01:52:55 +0200

    added new method to the class and changer implementation

--- src/main/java/net/lebedko/dao/connection/ThreadLocalConnectionProvider.java
+++ src/main/java/net/lebedko/dao/connection/ThreadLocalConnectionProvider.java
@@ -1,39 +1,36 @@
 package net.lebedko.dao.connection;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import static java.util.Objects.requireNonNull;
+import static java.util.Objects.isNull;
 
 /**
- * alexandr.lebedko : 08.05.2017.
+ * alexandr.lebedko : 04.07.2017.
  */
-
 public class ThreadLocalConnectionProvider implements ConnectionProvider {
-    private static final Logger logger = LogManager.getLogger();
-    private static final ThreadLocal<Connection> threadLocalConnection = new ThreadLocal<>();
-
+    private static final ThreadLocal<Connection> connectionThreadLocal = new ThreadLocal();
     private ConnectionProvider connectionProvider;
 
+    public ThreadLocalConnectionProvider() {
+        this(ConnectionProvider.getProvider());
+    }
+
     public ThreadLocalConnectionProvider(ConnectionProvider connectionProvider) {
-        this.connectionProvider = requireNonNull(connectionProvider,
-                "Connection Provider cannot be null!");
+        this.connectionProvider = connectionProvider;
     }
 
     @Override
     public Connection getConnection() throws SQLException {
-        logger.info("Connection requested");
-
-        if (threadLocalConnection.get() == null) {
-            Connection connection = requireNonNull(connectionProvider.getConnection(), "Connection cannot be null!");
-            threadLocalConnection.set(connection);
-
-            logger.info("New connection instance is set: " + connection);
+        if (isNull(connectionThreadLocal.get())) {
+            Connection connection = connectionProvider.getConnection();
+            connectionThreadLocal.set(connection);
         }
-        return new ThreadLocalConnectionWrapper(this.threadLocalConnection);
+
+        return connectionThreadLocal.get();
     }
 
+    public void clearStorage() {
+        connectionThreadLocal.remove();
+    }
 }
