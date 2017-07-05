f10f33de42beb834b7767753f5e6f4bdf060441a
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-07-05 02:02:06 +0200

    added class for counting nested  transactions

---
+++ src/main/java/net/lebedko/dao/transaction/TransactionCounter.java
@@ -0,0 +1,35 @@
+package net.lebedko.dao.transaction;
+
+import java.util.Stack;
+
+
+/**
+ * alexandr.lebedko : 04.07.2017.
+ */
+public class TransactionCounter {
+
+
+    //value to push in stack when starting or joining transaction
+    private static final Object CHIP = new Object();
+
+    private Stack chipStack = new Stack();
+
+    public void addTx() {
+        chipStack.push(CHIP);
+    }
+
+    public void removeTx() {
+        if (chipStack.empty()) {
+            throw new IllegalStateException("Counter is empty!");
+        }
+        chipStack.pop();
+    }
+
+    public boolean isNestedTx() {
+        if (chipStack.empty()) {
+            throw new IllegalStateException("Counter is empty!");
+        }
+
+        return chipStack.size() > 1;
+    }
+}
