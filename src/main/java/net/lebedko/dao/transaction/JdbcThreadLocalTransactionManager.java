9ef4be14926f92cadb2f2ebe66d52da5fc48bc79
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-07-05 02:03:29 +0200

    added proper transaction manager impl

---
+++ src/main/java/net/lebedko/dao/transaction/JdbcThreadLocalTransactionManager.java
@@ -0,0 +1,88 @@
+package net.lebedko.dao.transaction;
+
+import net.lebedko.dao.connection.ThreadLocalConnectionProvider;
+import net.lebedko.dao.exception.DataAccessException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static java.util.Objects.isNull;
+
+/**
+ * alexandr.lebedko : 04.07.2017.
+ */
+public final class JdbcThreadLocalTransactionManager extends TransactionManager {
+    private static final ThreadLocal<TransactionCounter> transactionCounter = new ThreadLocal<>();
+
+    private final ThreadLocalConnectionProvider connectionProvider;
+
+    public JdbcThreadLocalTransactionManager(ThreadLocalConnectionProvider connectionProvider) {
+        this.connectionProvider = connectionProvider;
+    }
+
+    @Override
+    protected void begin() throws DataAccessException {
+        LOG.debug("Begin transaction invoked");
+
+        if (isNull(transactionCounter.get())) {
+            transactionCounter.set(new TransactionCounter());
+            try {
+                LOG.debug("Creating new connection");
+                Connection connection = connectionProvider.getConnection();
+                connection.setAutoCommit(false);
+            } catch (SQLException e) {
+                LOG.error("Failed to create connection");
+                throw new DataAccessException(e);
+            }
+        }
+
+        transactionCounter.get().addTx();
+
+        LOG.debug("Begin transaction method executed");
+    }
+
+    @Override
+    protected void rollback() throws DataAccessException {
+        LOG.debug("Transaction rollback invoked");
+        try {
+            connectionProvider.getConnection().rollback();
+            connectionProvider.getConnection().setAutoCommit(true);
+        } catch (SQLException e) {
+            LOG.error("Failed to rollback transaction");
+            throw new DataAccessException(e);
+        } finally {
+            cleanUp();
+        }
+        LOG.debug("Transaction rollback executed");
+    }
+
+    @Override
+    protected void commit() throws DataAccessException {
+        LOG.debug("Commit method invoked");
+
+        if (transactionCounter.get().isNestedTx()) {
+            transactionCounter.get().removeTx();
+            LOG.debug("Committing of nested transaction amended");
+            return;
+        }
+
+        try {
+            connectionProvider.getConnection().commit();
+            connectionProvider.getConnection().setAutoCommit(true);
+            LOG.debug("Committing initial transaction");
+        } catch (SQLException e) {
+            LOG.error("Failed to commit transaction");
+            throw new DataAccessException(e);
+        } finally {
+            cleanUp();
+        }
+
+
+    }
+
+    private void cleanUp() {
+        LOG.debug("Cleaning up thread local resources: TransactionCounter and ThreadLocalConnectionProvider");
+        transactionCounter.remove();
+        connectionProvider.clearStorage();
+    }
+}
