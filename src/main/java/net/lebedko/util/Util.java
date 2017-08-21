fb16befa9bafc4a8afc5335d3e1c51c02333b038
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-08-21 11:22:54 +0200

    added method to remove extra spaces from intrnationalized string

--- src/main/java/net/lebedko/util/Util.java
+++ src/main/java/net/lebedko/util/Util.java
@@ -2,6 +2,9 @@ package net.lebedko.util;
 
 
 import net.lebedko.entity.Validatable;
+import net.lebedko.entity.demo.general.StringI18N;
+
+import java.util.stream.Collectors;
 
 /**
  * alexandr.lebedko : 27.04.2017.
@@ -17,6 +20,18 @@ public class Util {
 
     public static String removeExtraSpaces(String value) {
         return value.replaceAll("[\\s]{2,}", " ");
+    }
+
+    public static StringI18N removeExtraSpaces(StringI18N value) {
+        return new StringI18N(
+                value.getMap().entrySet().stream()
+                        .collect(Collectors.toMap(
+                                entry -> entry.getKey(),
+                                entry -> removeExtraSpaces(entry.getValue()))));
+    }
+
+    public static boolean isInRange(int value, int min, int max) {
+        return value >= min && value <= max;
     }
 
 }
