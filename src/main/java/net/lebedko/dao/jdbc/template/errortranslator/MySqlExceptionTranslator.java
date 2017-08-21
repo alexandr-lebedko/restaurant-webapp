0ba177609281964fa0b9c4b004a8791536dd2348
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-08-21 10:40:05 +0200

    added Logging to error translator

--- src/main/java/net/lebedko/dao/jdbc/template/errortranslator/MySqlExceptionTranslator.java
+++ src/main/java/net/lebedko/dao/jdbc/template/errortranslator/MySqlExceptionTranslator.java
@@ -2,6 +2,8 @@ package net.lebedko.dao.jdbc.template.errortranslator;
 
 import net.lebedko.dao.exception.DataAccessException;
 import net.lebedko.dao.exception.UniqueViolationException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.SQLException;
 
@@ -9,15 +11,20 @@ import java.sql.SQLException;
  * alexandr.lebedko : 10.05.2017.
  */
 public class MySqlExceptionTranslator implements ExceptionTranslator {
+    private static final Logger LOG = LogManager.getLogger();
 
     private static final int DUPLICATE_ENTRY = 1062;
     private static final int DUPLICATE_KEY = 1022;
 
     @Override
     public DataAccessException translate(SQLException e) {
-        if(e.getErrorCode()==DUPLICATE_ENTRY)
+        int errorCode = e.getErrorCode();
+
+        LOG.error("MySQL ERROR CODE: " + errorCode);
+
+        if (errorCode == DUPLICATE_ENTRY)
             return new UniqueViolationException(e);
-        if(e.getErrorCode()==DUPLICATE_KEY)
+        if (errorCode == DUPLICATE_KEY)
             return new UniqueViolationException(e);
 
         return new DataAccessException(e);
