0c1df7ad4071bbe61b2b6b097eccd71fd8123a50
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-08-21 12:18:09 +0200

    renamed response constant and used new util constant class

--- src/main/java/net/lebedko/web/command/impl/admin/AdminMainGetCommand.java
+++ src/main/java/net/lebedko/web/command/impl/admin/AdminMainGetCommand.java
@@ -5,14 +5,17 @@ import net.lebedko.web.command.IContext;
 import net.lebedko.web.command.impl.AbstractCommand;
 import net.lebedko.web.response.ForwardAction;
 import net.lebedko.web.response.IResponseAction;
-import net.lebedko.web.util.constant.PageLocations;
+
+import static net.lebedko.web.util.constant.WebConstant.*;
 
 /**
  * alexandr.lebedko : 24.07.2017.
  */
-public class AdminMainGetCommand extends AbstractCommand{
+public class AdminMainGetCommand extends AbstractCommand {
+    private static final IResponseAction MAIN_PAGE_FORWARD = new ForwardAction(PAGE.ADMIN_MENU);
+
     @Override
     protected IResponseAction doExecute(IContext context) throws ServiceException {
-        return new ForwardAction(PageLocations.MAIN_ADMIN);
+        return MAIN_PAGE_FORWARD;
     }
 }
