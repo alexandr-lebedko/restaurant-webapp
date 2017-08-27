b952c0152f005f8ab1cf4f21ba3a2bc69d9d7126
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-08-27 11:34:19 +0200

    refactored method: split in two

--- src/main/java/net/lebedko/web/validator/CategoryValidator.java
+++ src/main/java/net/lebedko/web/validator/CategoryValidator.java
@@ -1,31 +1,30 @@
 package net.lebedko.web.validator;
 
 import net.lebedko.entity.demo.item.Category;
+import net.lebedko.i18n.SupportedLocales;
 import net.lebedko.web.util.constant.PageErrorNames;
 
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
 
+import static net.lebedko.i18n.SupportedLocales.getLocales;
+
 /**
  * alexandr.lebedko : 04.08.2017.
  */
 public class CategoryValidator implements IValidator<Category> {
-    private final static Set<Locale> SUPPORTED_LOCALES = new HashSet<>();
-
-    static {
-        SUPPORTED_LOCALES.add(Locale.ENGLISH);
-        SUPPORTED_LOCALES.add(new Locale("ukr"));
-        SUPPORTED_LOCALES.add(new Locale("ru"));
-    }
 
 
     @Override
     public void validate(Category category, Errors errors) {
-        if(category.getValue().getMap().keySet().containsAll(SUPPORTED_LOCALES) && category.isValid()){
+        if (isContainsAllSupportedLocales(category) && category.isValid()) {
             return;
         }
         errors.register("invalid-category", PageErrorNames.INVALID_CATEGORY);
+    }
 
+    private boolean isContainsAllSupportedLocales(Category category) {
+        return category.getValue().getMap().keySet().containsAll(getLocales().values());
     }
 }
