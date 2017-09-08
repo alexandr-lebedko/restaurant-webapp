62c70d206dda6699be62a9be936e0a483dbb3442
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-09-08 16:34:27 +0200

    fixup! added new error messages for MenuItem form

--- src/main/java/net/lebedko/web/util/constant/PageErrorNames.java
+++ src/main/java/net/lebedko/web/util/constant/PageErrorNames.java
@@ -11,8 +11,21 @@ public class PageErrorNames {
     public static final String USER_NOT_EXISTS = "page.error.userNotExists";
     public static final String WRONG_PASSWORD = "page.error.wrongPassword";
     public static final String INVALID_USER = "page.error.invalidUser";
+    public static final String NULL_CATEGORY = "page.error.nullCategory";
     public static final String INVALID_CATEGORY = "page.error.invalidCategory";
     public static final String CATEGORY_EXISTS = "page.error.categoryExists";
     public static final String INVALID_IMAGE = "page.error.invalidImage";
     public static final String IMAGE_ERROR = "page.error.imageError";
+    public static final String INVALID_PRICE = "page.error.invalidPrice";
+    public static final String INVALID_TITLE = "page.error.invalidTitle";
+    public static final String NULL_TITLE = "page.error.nullTitle";
+    public static final String NULL_DESCRIPTION = "page.error.nullDescription";
+    public static final String INVALID_DESCRIPTION = "page.error.invalidDescription";
+    public static final String NULL_ITEM_INFO = "page.error.nullItemInfo";
+    public static final String NULL_ITEM_STATE = "page.error.nullItemState";
+    public static final String NULL_MENU_ITEM = "page.error.nullItem";
+    public static final String ITEM_EXISTS = "page.error.itemTitleExists";
+
+    //TODO
+
 }
