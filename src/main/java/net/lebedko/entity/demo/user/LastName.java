a199017bc652ee8b286132a3acfbfff1505d8759
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-09-09 13:42:58 +0200

    removed unused demo entities

--- src/main/java/net/lebedko/entity/demo/user/LastName.java
+++
@@ -1,7 +0,0 @@
-package net.lebedko.entity.demo.user;
-
-/**
- * alexandr.lebedko : 30.07.2017.
- */
-public class LastName {
-}
--- src/main/java/net/lebedko/entity/demo/user/UserRole.java
+++
@@ -1,9 +0,0 @@
-package net.lebedko.entity.demo.user;
-
-/**
- * alexandr.lebedko : 30.07.2017.
- */
-
-public enum UserRole {
-    CLIENT, ADMIN;
-}
