17ffc4600b9f0c9ab6ea3176580fb958cba0be63
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-09-25 18:09:20 +0200

    added new page and command to display it with needed constants

---
+++ src/main/java/net/lebedko/web/command/impl/client/ItemsByCategoryGetCommand.java
@@ -0,0 +1,99 @@
+package net.lebedko.web.command.impl.client;
+
+import net.lebedko.entity.demo.item.Category;
+import net.lebedko.entity.demo.item.CategoryView;
+import net.lebedko.entity.demo.item.MenuItemView;
+import net.lebedko.i18n.SupportedLocales;
+import net.lebedko.service.demo.CategoryService;
+import net.lebedko.service.demo.MenuItemService;
+import net.lebedko.service.exception.ServiceException;
+import net.lebedko.web.command.IContext;
+import net.lebedko.web.command.impl.AbstractCommand;
+import net.lebedko.web.response.ForwardAction;
+import net.lebedko.web.response.IResponseAction;
+import net.lebedko.web.response.RedirectAction;
+import net.lebedko.web.util.constant.WebConstant;
+import net.lebedko.web.validator.Errors;
+
+import java.util.Collection;
+import java.util.Locale;
+
+import static java.util.Objects.isNull;
+import static java.util.Optional.ofNullable;
+
+/**
+ * alexandr.lebedko : 18.09.2017.
+ */
+public class ItemsByCategoryGetCommand extends AbstractCommand {
+    private static final IResponseAction CATEGORIES_REDIRECT = new RedirectAction(WebConstant.URL.CLIENT_MAIN);
+    private static final IResponseAction ITEMS_FORWARD = new ForwardAction(WebConstant.PAGE.CLIENT_MENU_ITEMS);
+
+    private CategoryService categoryService;
+    private MenuItemService itemService;
+
+    public ItemsByCategoryGetCommand(CategoryService categoryService, MenuItemService itemService) {
+        this.categoryService = categoryService;
+        this.itemService = itemService;
+    }
+
+    @Override
+    protected IResponseAction doExecute(final IContext context) throws ServiceException {
+
+        final Errors errors = new Errors();
+
+        final Category category = getCategory(context, errors);
+
+        if (errors.hasErrors()) {
+            context.addRequestAttribute("categories", getCategories(context));
+            context.addErrors(errors);
+            //TODO: or maybe redirect to 404 page??
+            return CATEGORIES_REDIRECT;
+        }
+
+        context.addRequestAttribute("items", getItems(context, category));
+
+        return ITEMS_FORWARD;
+    }
+
+    private Category getCategory(final IContext context, final Errors errors) throws ServiceException {
+        final Integer categoryId = ofNullable(getCategoryId(context)).orElse(-1);
+
+        final Category category = categoryService.getByID(categoryId);
+
+        if (isNull(category)) {
+            //TODO: localize error
+            LOG.error("CATEGORY WITH CURRENT ID NOT EXISTS");
+            errors.register("noSuchCategory", "CATEGORY WITH CURRENT ID NOT EXISTS");
+        }
+
+        return category;
+    }
+
+    private Integer getCategoryId(final IContext context) {
+        Integer categoryId = null;
+        try {
+            return Integer.valueOf(context.getRequestParameter("category"));
+        } catch (NumberFormatException e) {
+            LOG.error(e);
+        }
+
+        if (isNull(categoryId)) {
+            try {
+                return Integer.valueOf(context.getRequestAttribute("category"));
+            } catch (NumberFormatException e) {
+                LOG.error(e);
+            }
+        }
+        return categoryId;
+    }
+
+    private Collection<MenuItemView> getItems(final IContext context, final Category category) throws ServiceException {
+        return itemService.getByCategory(category, context.getSessionAttribute(Locale.class, SupportedLocales.getLocaleSessionAttributeName()));
+    }
+
+    private Collection<CategoryView> getCategories(final IContext context) throws ServiceException {
+        return categoryService.getAll(context.getSessionAttribute(Locale.class, SupportedLocales.getLocaleSessionAttributeName()));
+    }
+
+
+}
--- src/main/java/net/lebedko/web/util/constant/WebConstant.java
+++ src/main/java/net/lebedko/web/util/constant/WebConstant.java
@@ -16,7 +16,9 @@ public final class WebConstant {
         public static final String ADMIN_MENU = PREFIX + "admin/menu" + SUFFIX;
         public static final String NEW_CATEGORY = PREFIX + "admin/newCategory" + SUFFIX;
         public static final String NEW_ITEM_PAGE = PREFIX + "admin/newItem" + SUFFIX;
-        public static final String CLIENT_MAIN =PREFIX +  "client/categories" + SUFFIX;
+        public static final String CLIENT_MAIN = PREFIX + "client/categories" + SUFFIX;
+        public static final String CLIENT_MENU_ITEMS = PREFIX + "client/items" + SUFFIX;
+        public static final String CLIENT_ORDER_FORM = PREFIX + "client/orderForm" + SUFFIX;
     }
 
     public static final class URL {
@@ -36,15 +38,20 @@ public final class WebConstant {
 
         public static final String ADMIN_MAIN = PREFIX + "admin/main";
         public static final String CLIENT_MAIN = PREFIX + "client/categories";
+        public static final String CLIENT_MENU_ITEMS = PREFIX + "client/items";
+        public static final String CLIENT_ORDER_FORM = PREFIX +"client/orderForm";
     }
 
     public static final class COMMAND {
         public static final String GET_PREFIX = "GET:";
         public static final String POST_PREFIX = "POST:";
 
-        public static final String SIGN_OUT=GET_PREFIX + URL.SIGN_OUT;
+        public static final String SIGN_OUT = GET_PREFIX + URL.SIGN_OUT;
 
-        public static final String GET_CLIENT_MAIN=GET_PREFIX+URL.CLIENT_MAIN;
+        public static final String GET_CLIENT_MAIN = GET_PREFIX + URL.CLIENT_MAIN;
+        public static final String GET_CLIENT_MENU_ITEMS = GET_PREFIX + URL.CLIENT_MENU_ITEMS;
+        public static final String POST_CLIENT_MENU_ITEMS = POST_PREFIX + URL.CLIENT_MENU_ITEMS;
+        public static final String GET_CLIENT_ORDER= GET_PREFIX+ URL.CLIENT_ORDER_FORM;
 
         public static final String GET_ADMIN_MENU = GET_PREFIX + URL.ADMIN_MENU;
         public static final String GET_ADMIN_NEW_CATEGORY = GET_PREFIX + URL.ADMIN_NEW_CATEGORY;
