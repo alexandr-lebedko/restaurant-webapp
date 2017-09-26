95a64069c2d1ce547deebc3c79b662e941794ada
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-09-26 11:01:08 +0200

    added command forwarding to order form page

---
+++ src/main/java/net/lebedko/web/command/impl/client/OrderGetCommand.java
@@ -0,0 +1,20 @@
+package net.lebedko.web.command.impl.client;
+
+import net.lebedko.service.exception.ServiceException;
+import net.lebedko.web.command.IContext;
+import net.lebedko.web.command.impl.AbstractCommand;
+import net.lebedko.web.response.ForwardAction;
+import net.lebedko.web.response.IResponseAction;
+import net.lebedko.web.util.constant.WebConstant.PAGE;
+
+/**
+ * alexandr.lebedko : 20.09.2017.
+ */
+public class OrderGetCommand extends AbstractCommand {
+    private static final IResponseAction ORDER_FORM_FORWARD = new ForwardAction(PAGE.CLIENT_ORDER_FORM);
+
+    @Override
+    protected IResponseAction doExecute(IContext context) throws ServiceException {
+        return ORDER_FORM_FORWARD;
+    }
+}
