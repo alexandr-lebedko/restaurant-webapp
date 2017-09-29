03ac901cc38b56da56ed602ddc5b9d86ce1006d1
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-09-29 10:17:26 +0200

    deleted class implemenetation

--- src/main/java/net/lebedko/entity/demo/invoice/Invoice.java
+++ src/main/java/net/lebedko/entity/demo/invoice/Invoice.java
@@ -1,20 +1,4 @@
 package net.lebedko.entity.demo.invoice;
 
-import net.lebedko.entity.order.Order;
-import net.lebedko.entity.user.User;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-/**
- * alexandr.lebedko : 30.07.2017.
- */
 public class Invoice {
-
-    private long id;
-    private User user;
-    private List<Order> orders;
-
-    private LocalDateTime creationDateTime;
-
 }
