f8acd2d408bd927f37f4f98d36f784657dc2ed41
Alexandr Lebedko <lebedko.alexandr@gmail.com>
2017-09-29 10:22:08 +0200

    stub implementation

---
+++ src/main/java/net/lebedko/web/validator/order/OrderValidator.java
@@ -0,0 +1,27 @@
+package net.lebedko.web.validator.order;
+
+import net.lebedko.entity.demo.order.Order;
+import net.lebedko.web.validator.Errors;
+import net.lebedko.web.validator.IValidator;
+import net.lebedko.web.validator.item.MenuItemValidator;
+
+
+/**
+ * alexandr.lebedko : 24.09.2017.
+ */
+public class OrderValidator implements IValidator<Order> {
+    private MenuItemValidator itemValidator;
+
+    public OrderValidator(MenuItemValidator itemValidator) {
+        this.itemValidator = itemValidator;
+    }
+
+    public OrderValidator() {
+        this.itemValidator = new MenuItemValidator();
+    }
+
+    @Override
+    public void validate(Order order, Errors errors) {
+
+    }
+}
