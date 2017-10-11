01f13da895fb3f35a8171afb7604429a62ba82ab
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-10-11 10:52:34 +0200

    changed access modificator on session/request locale attribute name

--- src/main/java/net/lebedko/i18n/SupportedLocales.java
+++ src/main/java/net/lebedko/i18n/SupportedLocales.java
@@ -6,19 +6,19 @@ import java.util.*;
  * alexandr.lebedko : 22.08.2017.
  */
 public final class SupportedLocales {
-    public static final String UA_CODE = "uk_UA";
+    public static final String UA_CODE = "ua";
     public static final String EN_CODE = "en";
-    public static final String RU_CODE = "ru_RU";
+    public static final String RU_CODE = "ru";
 
     private static final Map<String, Locale> LOCALES = new HashMap<>();
     private static final Locale DEFAULT_LOCALE = new Locale(RU_CODE);
-    private static final String LOCALE_SESSION_ATTRIBUTE_NAME = "lang";
-    private static final String LOCALE_REQUEST_ATTRIBUTE_NAME = "lang";
+    public static final String LOCALE_SESSION_ATTRIBUTE_NAME = "lang";
+    public static final String LOCALE_REQUEST_ATTRIBUTE_NAME = "lang";
 
     static {
         LOCALES.put(EN_CODE, Locale.ENGLISH);
         LOCALES.put(UA_CODE, new Locale(UA_CODE));
-        LOCALES.put(RU_CODE, new Locale(RU_CODE));
+        LOCALES.put(RU_CODE, DEFAULT_LOCALE);
     }
 
     private SupportedLocales() {
