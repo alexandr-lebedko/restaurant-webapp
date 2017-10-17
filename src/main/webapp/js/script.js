209aacb57e466e9157cc9c4971b59b631cb96693
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-10-17 10:05:51 +0200

    added ability to send post reqhuest on click

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,9 +1,23 @@
-$(document).ready(function () {
-    var contentHeight = $("#admin-middle-section").height();
-    console.log("CONTENT HEIGHT: " + contentHeight);
-    $('#side-bar').height(contentHeight);
-}).get();
-
 function previewImage(event, destinationImageId) {
     document.getElementById(destinationImageId).src = URL.createObjectURL(event.target.files[0]);
+}
+
+function post(path, params) {
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", path);
+
+    for (var key in params) {
+        if (params.hasOwnProperty(key)) {
+            var field = document.createElement("input");
+            field.setAttribute("type", "hidden");
+            field.setAttribute("name", key);
+            field.setAttribute("value", params[key]);
+
+            form.appendChild(field);
+        }
+
+        document.body.appendChild(form);
+        form.submit();
+    }
 }
