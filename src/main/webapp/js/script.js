aea2d6619ce487d7f238897b1beb422ffcbe7fbf
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-10-22 18:40:59 +0200

    functions for modifying, deleting order items and for building and sending form to server

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -21,3 +21,50 @@ function post(path, params) {
         form.submit();
     }
 }
+
+function deleteItemRow(rowSelector) {
+    const dishRow = document.querySelector("#".concat(rowSelector));
+
+    dishRow.parentNode.removeChild(dishRow);
+}
+
+function minusItemBasket(dishAmountSelector) {
+    const dishDiv = document.querySelector("#".concat(dishAmountSelector));
+
+    if (dishDiv.innerText > 1) {
+        dishDiv.innerText--;
+    }
+}
+
+function plusItemBasket(dishAmountSelector) {
+    const dishDiv = document.querySelector("#".concat(dishAmountSelector));
+
+    if (isNaN(dishDiv.innerText)) {
+        dishDiv.innerText = 1;
+    }
+    const value = dishDiv.innerText++;
+}
+
+function submitOrder() {
+    const form = document.querySelector("form");
+    console.log(form);
+
+    const itemIds = document.querySelectorAll(".item-id");
+    const itemAmounts = document.querySelectorAll(".item-amount");
+
+    for (let k = 0; k < itemIds.length; k++) {
+        const itemIdInput = document.createElement('input');
+        itemIdInput.name = "item-id";
+        itemIdInput.value = itemIds[k].innerText;
+        itemIdInput.type = "hidden";
+
+        const itemAmountInput = document.createElement('input');
+        itemAmountInput.name = "item-amount";
+        itemAmountInput.value = itemAmounts[k].innerText;
+        itemAmountInput.type = "hidden";
+
+        form.appendChild(itemIdInput);
+        form.appendChild(itemAmountInput);
+    }
+    form.submit();
+}
