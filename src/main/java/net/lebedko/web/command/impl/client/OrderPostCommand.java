28275b2345dd92939bd0dedfdac1921c9d3a8ce1
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-10-27 13:07:33 +0200

    refactored command implementation

--- src/main/java/net/lebedko/web/command/impl/client/OrderPostCommand.java
+++ src/main/java/net/lebedko/web/command/impl/client/OrderPostCommand.java
@@ -4,28 +4,36 @@ import net.lebedko.entity.item.Item;
 import net.lebedko.entity.order.Order;
 import net.lebedko.entity.user.User;
 import net.lebedko.service.OrderService;
+import net.lebedko.service.exception.ClosedInvoiceException;
 import net.lebedko.service.exception.ServiceException;
 import net.lebedko.web.command.IContext;
 import net.lebedko.web.command.impl.AbstractCommand;
 import net.lebedko.web.response.ForwardAction;
 import net.lebedko.web.response.IResponseAction;
 import net.lebedko.web.response.RedirectAction;
+import net.lebedko.web.util.constant.PageErrorNames;
+import net.lebedko.web.util.constant.URL;
 import net.lebedko.web.validator.Errors;
 import net.lebedko.web.validator.order.OrderValidator;
 
 
 import java.util.*;
+import java.util.stream.IntStream;
 
-import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+import static net.lebedko.web.util.constant.PageErrorNames.ORDER_FORM_ERROR;
+import static net.lebedko.web.util.constant.PageErrorNames.UNPAID_INVOICE;
 import static net.lebedko.web.util.constant.WebConstant.*;
 
 /**
  * alexandr.lebedko : 20.09.2017.
  */
+
 public class OrderPostCommand extends AbstractCommand {
     private static final IResponseAction ORDER_FORM_FORWARD = new ForwardAction(PAGE.CLIENT_ORDER_FORM);
-    private static final IResponseAction SUCCESS_PAGE_REDIRECT = new RedirectAction(URL.SUCCESS_ORDER);
+    private static final IResponseAction ORDER_FORM_REDIRECT = new RedirectAction(URL.CLIENT_ORDER_FORM);
+    private static final IResponseAction ORDERS_REDIRECT = new RedirectAction(URL.CLIENT_ORDERS);
 
     private OrderService orderService;
     private OrderValidator validator;
@@ -39,63 +47,74 @@ public class OrderPostCommand extends AbstractCommand {
     protected IResponseAction doExecute(IContext context) throws ServiceException {
         final Errors errors = new Errors();
 
-        final User user = getUserFromSession(context);
-        final Map<Item, Integer> orderContent = getOrderContent(context);
-
-        validator.validate(orderContent, errors);
+        Map<Long, Integer> amountToIds = parseOrderForm(context, errors);
 
         if (errors.hasErrors()) {
-            LOG.warn("Attempt to create invalid Order. Order Content: isEmpty:" + orderContent.isEmpty() + ", content: " + orderContent);
             context.addErrors(errors);
             return ORDER_FORM_FORWARD;
         }
 
-        final Order order = orderService.createOrder(user, orderContent);
-        context.addRequestAttribute("order", order);
+        if (amountToIds.isEmpty()) {
+            clearOrderBucketInfo(context);
+            return ORDER_FORM_REDIRECT;
+        }
 
-        return SUCCESS_PAGE_REDIRECT;
+        Map<Item, Integer> orderContent = orderService.toOrderContent(amountToIds);
+
+        if (validator.notValid(orderContent, errors)) {
+            context.addErrors(errors);
+            return ORDER_FORM_FORWARD;
+        }
+
+        try {
+            orderService.createOrder(getUserFromSession(context), orderContent);
+            clearOrderBucketInfo(context);
+
+            return ORDERS_REDIRECT;
+        } catch (ClosedInvoiceException cie) {
+            LOG.error(cie);
+            errors.register("unpaidInvoice", UNPAID_INVOICE);
+        }
+
+        context.addErrors(errors);
+
+        return ORDER_FORM_FORWARD;
+    }
+
+    private void clearOrderBucketInfo(IContext context) {
+        context.removeSessionAttribute("orderContent");
+        context.removeSessionAttribute("orderAmount");
     }
 
     private User getUserFromSession(IContext context) {
         return context.getSessionAttribute(User.class, "user");
     }
 
-    private Map<Item, Integer> getOrderContent(IContext context) {
-        final Map<Item, Integer> orderContent = getOrderContentFromSession(context);
-        final List<Long> itemIds = getItemIdsFromForm(context);
-        final List<Integer> quantities = getItemQuantitiesForm(context);
+    private Map<Long, Integer> parseOrderForm(IContext context, Errors errors) {
+        final List<Long> ids = getIds(context);
+        final List<Integer> amounts = getAmounts(context);
 
-        if (itemIds.size() != quantities.size()) {
-            //TODO :: this case may happen only if form is forged
-            return Collections.emptyMap();
+        if (ids.size() != amounts.size()) {
+            errors.register("formError", ORDER_FORM_ERROR);
+            return null;
         }
-        for (int i = 0; i < itemIds.size(); i++) {
-            Long id = itemIds.get(i);
-            Integer quantity = quantities.get(i);
 
-            orderContent.keySet().stream()
-                    .filter(item -> id.equals(item.getId()))
-                    .findFirst()
-                    .ifPresent(item -> orderContent.put(item, quantity));
-        }
-        return orderContent;
+        return IntStream.range(0, ids.size())
+                .boxed()
+                .collect(toMap(ids::get, amounts::get));
     }
 
-    private List<Integer> getItemQuantitiesForm(IContext context) {
-        return context.getRequestParameters("quantity").stream()
+
+    private List<Integer> getAmounts(IContext context) {
+        return context.getRequestParameters("item-amount").stream()
                 .map(this::parseItemQuantity)
                 .collect(toList());
     }
 
-    private List<Long> getItemIdsFromForm(IContext context) {
-        return context.getRequestParameters("itemId").stream()
+    private List<Long> getIds(IContext context) {
+        return context.getRequestParameters("item-id").stream()
                 .map(this::parseItemId)
                 .collect(toList());
-    }
-
-    @SuppressWarnings("unchecked")
-    private Map<Item, Integer> getOrderContentFromSession(IContext context) {
-        return ofNullable(context.getSessionAttribute(Map.class, "orderContent")).orElse(new HashMap());
     }
 
     private long parseItemId(String value) {
