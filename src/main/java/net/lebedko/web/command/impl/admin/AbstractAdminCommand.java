4a03f87b456bb2762f89dd16fe47d7cc7f49e4aa
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-11-03 21:06:07 +0200

    added abstract class for all admin commands

---
+++ src/main/java/net/lebedko/web/command/impl/admin/AbstractAdminCommand.java
@@ -0,0 +1,49 @@
+package net.lebedko.web.command.impl.admin;
+
+import net.lebedko.entity.invoice.Invoice;
+import net.lebedko.entity.order.Order;
+import net.lebedko.service.InvoiceService;
+import net.lebedko.service.OrderService;
+import net.lebedko.service.exception.ServiceException;
+import net.lebedko.web.command.IContext;
+import net.lebedko.web.command.impl.AbstractCommand;
+import net.lebedko.web.response.IResponseAction;
+
+import java.util.Collection;
+
+import static java.util.Optional.ofNullable;
+
+public abstract class AbstractAdminCommand extends AbstractCommand {
+    private OrderService orderService;
+    private InvoiceService invoiceService;
+
+    public AbstractAdminCommand(OrderService orderService, InvoiceService invoiceService) {
+        this.orderService = orderService;
+        this.invoiceService = invoiceService;
+    }
+
+    @Override
+    protected final IResponseAction doExecute(IContext context) throws ServiceException {
+        IResponseAction responseAction = _doExecute(context);
+
+        addOrderAndInvoiceStatistics(context);
+
+        return responseAction;
+    }
+
+    private void addOrderAndInvoiceStatistics(IContext context) {
+
+        Collection<Invoice> unprocessedInvoices = invoiceService.getClosedInvoices();
+        if (!unprocessedInvoices.isEmpty()){
+            context.addRequestAttribute("unprocessedInvoiceNum", unprocessedInvoices.size());
+        }
+
+        Collection<Order> unprocessedOrders = orderService.getUnprocessedOrders();
+        if (!unprocessedOrders.isEmpty()) {
+            context.addRequestAttribute("unprocessedOrderNum", unprocessedOrders.size());
+        }
+    }
+
+
+    protected abstract IResponseAction _doExecute(IContext context) throws ServiceException;
+}
