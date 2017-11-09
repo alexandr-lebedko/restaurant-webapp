d82c43c7d40c0aabb6b690db25a3fc742e1d7e13
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-11-09 16:59:26 +0200

    added class for holding session/request/param attribute names

---
+++ src/main/java/net/lebedko/web/util/constant/Attribute.java
@@ -0,0 +1,10 @@
+package net.lebedko.web.util.constant;
+
+public class Attribute {
+    private Attribute() {
+    }
+
+    public static final String ORDER_ID = "orderId";
+    public static final String ORDER_DETAILS = "orderDetails";
+    public static final String ADMIN_NEW_ORDERS = "adminNewOrders";
+}
