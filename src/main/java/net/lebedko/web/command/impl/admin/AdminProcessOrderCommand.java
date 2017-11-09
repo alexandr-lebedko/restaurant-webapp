ea321b41863b75abd9ea59b2cf5ce093a6952ca0
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-11-09 19:15:36 +0200

    added commands to process and reject orders

---
+++ src/main/java/net/lebedko/web/command/impl/admin/AdminProcessOrderCommand.java
@@ -0,0 +1,40 @@
+package net.lebedko.web.command.impl.admin;
+
+import net.lebedko.service.InvoiceService;
+import net.lebedko.service.OrderService;
+import net.lebedko.service.exception.ServiceException;
+import net.lebedko.web.command.IContext;
+import net.lebedko.web.response.ForwardAction;
+import net.lebedko.web.response.IResponseAction;
+import net.lebedko.web.response.RedirectAction;
+import net.lebedko.web.util.constant.Attribute;
+import net.lebedko.web.util.constant.URL;
+
+public class AdminProcessOrderCommand extends AbstractAdminCommand {
+
+
+    public AdminProcessOrderCommand(OrderService orderService, InvoiceService invoiceService) {
+        super(orderService, invoiceService);
+    }
+
+    @Override
+    protected IResponseAction _doExecute(IContext context) throws ServiceException {
+        Long orderId = getOrderId(context);
+        orderService.processOrderById(orderId);
+
+        return new RedirectAction(URL.ADMIN_ORDER_DETAILS.concat("?").concat(Attribute.ORDER_ID).concat("=").concat(orderId.toString()));
+    }
+
+    private Long getOrderId(IContext context) {
+        Long id = -1L;
+        try {
+            id = Long.valueOf(context.getRequestParameter(Attribute.ORDER_ID));
+        } catch (NumberFormatException e) {
+            LOG.error(e);
+        }
+
+        return id;
+    }
+
+
+}
---
+++ src/main/java/net/lebedko/web/command/impl/admin/AdminRejectOrderCommand.java
@@ -0,0 +1,35 @@
+package net.lebedko.web.command.impl.admin;
+
+import net.lebedko.service.InvoiceService;
+import net.lebedko.service.OrderService;
+import net.lebedko.service.exception.ServiceException;
+import net.lebedko.web.command.IContext;
+import net.lebedko.web.response.IResponseAction;
+import net.lebedko.web.response.RedirectAction;
+import net.lebedko.web.util.constant.Attribute;
+import net.lebedko.web.util.constant.URL;
+
+public class AdminRejectOrderCommand extends AbstractAdminCommand {
+
+    public AdminRejectOrderCommand(OrderService orderService, InvoiceService invoiceService) {
+        super(orderService, invoiceService);
+    }
+
+    @Override
+    protected IResponseAction _doExecute(IContext context) throws ServiceException {
+        Long id = getOrderId(context);
+        orderService.rejectOrderById(id);
+
+        return new RedirectAction(URL.ADMIN_ORDER_DETAILS.concat("?").concat(Attribute.ORDER_ID).concat("=").concat(id.toString()));
+    }
+
+    private Long getOrderId(IContext context) {
+        Long id = -1L;
+        try {
+            id = Long.valueOf(context.getRequestParameter(Attribute.ORDER_ID));
+        } catch (NumberFormatException e) {
+            LOG.error(e);
+        }
+        return id;
+    }
+}
