59959c98910d2163f59e49b26c6978c326e984b3
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-11-19 22:21:12 +0200

    changed map type, and added objects to response

--- src/main/java/net/lebedko/web/command/impl/admin/AdminGetInvoiceCommand.java
+++ src/main/java/net/lebedko/web/command/impl/admin/AdminGetInvoiceCommand.java
@@ -1,6 +1,8 @@
 package net.lebedko.web.command.impl.admin;
 
 import net.lebedko.entity.invoice.Invoice;
+import net.lebedko.entity.item.Item;
+import net.lebedko.entity.order.Order;
 import net.lebedko.entity.order.OrderItem;
 import net.lebedko.service.InvoiceService;
 import net.lebedko.service.OrderService;
@@ -11,13 +13,17 @@ import net.lebedko.web.response.IResponseAction;
 import net.lebedko.web.util.CommandUtils;
 import net.lebedko.web.util.constant.Attribute;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Objects.nonNull;
 import static net.lebedko.web.util.constant.WebConstant.*;
 
 public class AdminGetInvoiceCommand extends AbstractAdminCommand {
     private static final IResponseAction INVOICE_FORWARD = new ForwardAction(PAGE.ADMIN_INVOICE);
+    private static final Comparator<Order> COMPARATOR = Comparator.comparing(Order::getCreatedOn).reversed();
+
 
     public AdminGetInvoiceCommand(OrderService orderService, InvoiceService invoiceService) {
         super(orderService, invoiceService);
@@ -26,11 +32,15 @@ public class AdminGetInvoiceCommand extends AbstractAdminCommand {
     @Override
     protected IResponseAction _doExecute(IContext context) throws ServiceException {
         final Invoice invoice = invoiceService.getInvoice(getInvoiceId(context));
-        if (nonNull(invoice)) {
-            final Collection<OrderItem> orderItems = orderService.getOrderItemsByInvoice(invoice);
-            context.addRequestAttribute(Attribute.INVOICE, invoice);
-            context.addRequestAttribute(Attribute.ORDER_ITEMS, orderItems);
-        }
+        final Collection<OrderItem> orderItems = orderService.getOrderItemsByInvoice(invoice);
+
+        final Map<Order, List<OrderItem>> itemsToOrder = new TreeMap<>(COMPARATOR);
+
+        itemsToOrder.putAll(orderItems.stream()
+                .collect(Collectors.groupingBy(OrderItem::getOrder)));
+
+        context.addRequestAttribute(Attribute.INVOICE, invoice);
+        context.addRequestAttribute(Attribute.ITEMS_TO_ORDER, itemsToOrder);
 
         return INVOICE_FORWARD;
     }
