50bd19b3ecbd5913b2ed0cf2b012ed5b5a7d579b
alexandr-lebedko <lebedko.alexandr@gmail.com>
2017-11-30 20:47:42 +0200

    refactored abstract command

--- src/main/java/net/lebedko/web/command/impl/AbstractCommand.java
+++ src/main/java/net/lebedko/web/command/impl/AbstractCommand.java
@@ -4,7 +4,6 @@ import net.lebedko.service.exception.ServiceException;
 import net.lebedko.web.command.ICommand;
 import net.lebedko.web.command.IContext;
 import net.lebedko.web.response.IResponseAction;
-import net.lebedko.web.validator.Errors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
