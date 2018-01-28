30ef6e13480a33b3d189675593156b98d34f4c64
alexandr-lebedko <lebedko.alexandr@gmail.com>
2018-01-28 10:29:20 +0200

    fixed error message and removed unused constants

--- src/main/java/net/lebedko/web/util/constant/PageErrorNames.java
+++ src/main/java/net/lebedko/web/util/constant/PageErrorNames.java
@@ -7,27 +7,12 @@ public class PageErrorNames {
     public static final String USER_EXISTS = "page.error.userExists";
     public static final String USER_NOT_EXISTS = "page.error.userNotExists";
     public static final String WRONG_PASSWORD = "page.error.wrongPassword";
-    public static final String INVALID_USER = "page.error.invalidUser";
-    public static final String NULL_CATEGORY = "page.error.nullCategory";
     public static final String INVALID_CATEGORY = "page.error.invalidCategory";
     public static final String CATEGORY_EXISTS = "page.error.categoryExists";
     public static final String INVALID_IMAGE = "page.error.invalidImage";
-    public static final String IMAGE_ERROR = "page.error.imageError";
     public static final String INVALID_PRICE = "page.error.invalidPrice";
     public static final String INVALID_TITLE = "page.error.invalidTitle";
-    public static final String NULL_TITLE = "page.error.nullTitle";
-    public static final String NULL_DESCRIPTION = "page.error.nullDescription";
     public static final String INVALID_DESCRIPTION = "page.error.invalidDescription";
     public static final String INVOICE_PAY_ERROR = "invoice.close.error";
     public static final String ITEM_EXISTS = "page.error.itemTitleExists";
-    public static final String NULL_ITEM_INFO = "page.error.nullItemInfo";
-    public static final String NULL_ITEM_STATE = "page.error.nullItemState";
-    public static final String NULL_MENU_ITEM = "page.error.nullItem";
-    public static final String INVALID_ORDER = "page.error.invalidOrder";
-    public static final String INVALID_ORDER_POSITION = "page.error.invalidOrderPosition";
-    public static final String EMPTY_ORDER = "page.error.emptyOrder";
-    public static final String NEGATIVE_ITEM_QUANTITY = "page.error.negativeItemQuantity";
-    public static final String ORDER_FORM_ERROR = "page.error.order.form";
-    public static final String UNPAID_INVOICE = "page.error.invoice.unpaid";
-    public static final String INVALID_MODIFY_ORDER_FORM = "invalid.modify.order.form";
 }
